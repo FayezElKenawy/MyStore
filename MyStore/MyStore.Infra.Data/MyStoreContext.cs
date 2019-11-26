222eb8d96407f4e3f5d12a8f6635ad660b9f8786
Fayez Emad Elkenawy <fezoelkenawy24@yahoo.com>
2019-11-26 11:35:55 +0300

    intial commit for context

---
+++ MyStore/MyStore.Infra.Data/MyStoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using MyStore.Domain.Models;
+
+namespace MyStore.Infra.Data
+{
+   public class MyStoreContext:DbContext
+    {
+        public MyStoreContext(DbContextOptions options):base (options)
+        {
+            
+        }
+        public Product products { get; set; }
+
+    }
+}
