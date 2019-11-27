6a8a5600e9f138bcd1315c195c1a6c08d6fcd75a
Fayez Emad Elkenawy <fezoelkenawy24@yahoo.com>
2019-11-27 14:35:52 +0300

    adding controler and view and run mvc adding customers model

---
+++ MyStore/Mystore.MVC/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyStore.Application.Interfaces;
+using MyStore.Application.ViewModel;
+
+namespace Mystore.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            ProductViewModel model = _productService.GetProducts();
+            return View(model);
+        }
+    }
+}
