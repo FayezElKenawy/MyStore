3a70302bae832cb394abc8c0b655f20e4ea3bf1e
Fayez Emad Elkenawy <fezoelkenawy24@yahoo.com>
2019-11-27 09:38:03 +0300

    Adding ViewModel and ServiceInterface and Repository Interface

---
+++ MyStore/MyStore.Application/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using MyStore.Application.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Application.Interfaces
+{
+    public interface IProductService
+    {
+        IEnumerable<ProductViewModel> GetProducts();
+    }
+}
