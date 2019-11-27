bbf0aa656444189ec6ecbd54bec664af8a9547e4
Fayez Emad Elkenawy <fezoelkenawy24@yahoo.com>
2019-11-27 12:02:01 +0300

    adding ddependency container and configer presentation layer

---
+++ MyStore/MyStore.Infra.Ioc/DepenencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MyStore.Application.Interfaces;
+using MyStore.Application.Services;
+using MyStore.Domain.Interfaces;
+using MyStore.Infra.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Infra.Ioc
+{
+    public class DepenencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+        }
+    }
+}
--- MyStore/Mystore.MVC/Startup.cs
+++ MyStore/Mystore.MVC/Startup.cs
@@ -14,6 +14,7 @@ using Mystore.MVC.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyStore.Infra.Data;
+using MyStore.Infra.Ioc;
 
 namespace Mystore.MVC
 {
@@ -47,6 +48,7 @@ namespace Mystore.MVC
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -77,5 +79,10 @@ namespace Mystore.MVC
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DepenencyContainer.RegisterServices(services);
+        }
+
     }
 }
