4e462ed29a1695162d7354ad358d20905e2f8bd1
Fayez Emad Elkenawy <fezoelkenawy24@yahoo.com>
2019-12-02 09:23:17 +0300

    adding customer controller

---
+++ MyStore/Mystore.MVC/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyStore.Application.Interfaces;
+using MyStore.Application.ViewModel;
+
+namespace Mystore.MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ICustomerService _service;
+        public CustomerController(ICustomerService service)
+        {
+            _service = service;
+        }
+        public IActionResult Index()
+        {
+            CustomerViewModel model = _service.GetCustomers();
+            return View();
+        }
+    }
+}
