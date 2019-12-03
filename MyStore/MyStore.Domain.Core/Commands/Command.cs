53b9a344248e01dbb9c10ec39db53612ccdc9a43
Fayez Emad Elkenawy <fezoelkenawy24@yahoo.com>
2019-12-03 08:32:14 +0300

    adding domain .core project and adding class

---
+++ MyStore/MyStore.Domain.Core/Commands/Command.cs
@@ -0,0 +1,16 @@
+﻿using MyStore.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Domain.Core.Commands
+{
+    public abstract class Command:Message
+    {
+        public DateTime Time { get;protected set; }
+        protected Command()
+        {
+            Time = DateTime.Now;
+        }
+    }
+}
--- MyStore/MyStore.Infra.Bus/InMemoryBus.cs
+++ MyStore/MyStore.Infra.Bus/InMemoryBus.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using MediatR;
+using MyStore.Domain.Core.Bus;
+using MyStore.Domain.Core.Commands;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MyStore.Infra.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus:IMediatorHandler
     {
+        private IMediator _meadiatr;
+        public InMemoryBus(IMediator mediator)
+        {
+            _meadiatr = mediator;
+        }
+
+        public Task SendCommand<T> (T command) where T : Command
+        {
+            return _meadiatr.Send(command);
+        }
+
     }
 }
